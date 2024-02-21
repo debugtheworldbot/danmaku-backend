089b18f26fcd4f7dca9f79de203dc9e261e3d42a
tian <istiancz@gmail.com>
2024-02-21 16:42:20 +0800

    fix: api

--- app/youtube/api/route.ts
+++ app/youtube/api/route.ts
@@ -2,34 +2,37 @@ import { google } from "googleapis";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id") || "86Gy035z_KA";
-  const youtube = google.youtube({
-    version: "v3",
-    auth: process.env.GOOGLE_AUTH,
-  });
-  const list = await youtube.commentThreads.list({
-    part: ["snippet"],
-    maxResults: 100,
-    order: "relevance",
-    videoId: id,
-  });
-  const withTimeFlag = list.data.items?.filter((item) =>
-    item.snippet?.topLevelComment?.snippet?.textDisplay?.includes("href"),
-  );
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id") || "86Gy035z_KA";
+    const youtube = google.youtube({
+      version: "v3",
+      auth: process.env.GOOGLE_AUTH,
+    });
+    const list = await youtube.commentThreads.list({
+      part: ["snippet"],
+      maxResults: 100,
+      order: "relevance",
+      videoId: id,
+    });
+    const withTimeFlag = list.data.items?.filter((item) =>
+      item.snippet?.topLevelComment?.snippet?.textDisplay?.includes("href"),
+    );
 
-  const timeRegex = /;t=(\d+)/;
+    const timeRegex = /;t=(\d+)/;
 
-  const res = withTimeFlag?.map((item) => {
-    const time =
-      item.snippet?.topLevelComment?.snippet?.textDisplay?.match(timeRegex);
-    const timeStamp = parseInt(time?.[1] || "0");
+    const res = withTimeFlag?.map((item) => {
+      const time =
+        item.snippet?.topLevelComment?.snippet?.textDisplay?.match(timeRegex);
+      const timeStamp = parseInt(time?.[1] || "0");
 
-    return {
-      time: timeStamp,
-      text: item.snippet?.topLevelComment?.snippet?.textOriginal,
-    };
-  });
-
-  return NextResponse.json({ data: res }, { status: 200 });
+      return {
+        time: timeStamp,
+        text: item.snippet?.topLevelComment?.snippet?.textOriginal,
+      };
+    });
+    return NextResponse.json({ data: res }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ data: [] }, { status: 500 });
+  }
 }
